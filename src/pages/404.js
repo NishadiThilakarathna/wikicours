71cd0f8ae02e9f718e414e9d9af6c7506b5b2580
Thomas KUNTZ <thomaskuntzz@protonmail.com>
2018-05-04 11:53:35 +0200

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404—Page Not Found</h1>
+    <p>Aucune page n'existe à cette adresse... 😢</p>
+    <p style={{fontStyle: 'italic'}}>Vous pouvez utiliser la <a href='https://archive.org/web/'>Wayback Machine</a> de l'Internet Archive pour chercher une version archivée de cette page, dans le cas où elle aurait existé par le passé.</p>
+    <img style={{maxWidth: '100%'}} src='nyan-cat.gif' alt='Nyan Cat' />
   </div>
 )
 
